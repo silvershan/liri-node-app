bc3107cbc87699225dd3d6a541dc018f9f7d899a
silvershan <44039173+silvershan@users.noreply.github.com>
2019-02-21 16:54:33 -0800

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,18 @@
+//console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+exports.bandsintown = {
+    id: process.env.BANDSINTOWN_ID
+}
+
+exports.omdb = {
+    id: process.env.OMDB_ID
+}
+
+exports.mapquest = {
+    id: process.env.MAPQUEST_ID
+}
